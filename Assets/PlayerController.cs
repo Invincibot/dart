d1d06bfd7760c4dbedf9b3eaa26295b3c05c1ad4
vince <vincetiu8@gmail.com>
2021-03-24 16:11:03 +0800

    add player movement

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float rotateSpeed;
+
+    private Rigidbody2D _rigidbody2D;
+    private InputAction _moveAction;
+
+    private void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        
+        PlayerInput playerInput = GetComponent<PlayerInput>();
+        _moveAction = playerInput.actions["Move"];
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 moveDirection = _moveAction.ReadValue<Vector2>();
+        _rigidbody2D.AddRelativeForce(new Vector2(0, moveDirection.y * moveSpeed * Time.deltaTime));
+        _rigidbody2D.AddTorque(-moveDirection.x * rotateSpeed * Time.deltaTime);
+    }
+}
